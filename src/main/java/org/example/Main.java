f2e4d50c49db7a46fc1db0c8601f00da79da9d8b
josarapar <josearanda9000@gmail.com>
2025-12-12 20:10:10 +0100

    add guessing logic to number guessing game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,18 @@ public class Main {
             int secret = rnd.nextInt(100) + 1;
             System.out.println("Adivina el número (1-100):");
 
+            while (true) {
+
+                int guess = sc.nextInt();
+                if (guess < secret) {
+                    System.out.println("El número es mayor");
+                } else if (guess > secret) {
+                    System.out.println("El número es menor");
+                } else {
+                    System.out.println("Número correcto");
+                    break;
+                }
+            }
 
             System.out.println("¿Jugar de nuevo? (s/n)");
             String resp = sc.next();
