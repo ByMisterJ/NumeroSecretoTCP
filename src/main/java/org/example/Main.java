ec3f07dfb6be858bb9d84fed113e9ec00809ed80
josarapar <josearanda9000@gmail.com>
2025-12-12 20:08:36 +0100

    add main method with scanner and random initialization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,11 @@
 package org.example;
 
 public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        java.util.Random rnd = new java.util.Random();
+        boolean play = true;
 
+
+    }
 }
