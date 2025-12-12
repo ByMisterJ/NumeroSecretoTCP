2f0d51b254fad8c4a167685b57451641c4baa160
josarapar <josearanda9000@gmail.com>
2025-12-12 20:09:58 +0100

    add game loop for number guessing and scanner closure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,18 @@ public class Main {
         java.util.Random rnd = new java.util.Random();
         boolean play = true;
 
+        while (play) {
+            int secret = rnd.nextInt(100) + 1;
+            System.out.println("Adivina el número (1-100):");
 
+
+            System.out.println("¿Jugar de nuevo? (s/n)");
+            String resp = sc.next();
+            if (!resp.equalsIgnoreCase("s")) {
+                play = false;
+            }
+        }
+
+        sc.close(); // cerrar scanner
     }
 }
