faa403c039fc321d400d0213f155a7f25f1e1200
josarapar <josearanda9000@gmail.com>
2025-12-12 20:11:11 +0100

    change main method to static in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
         java.util.Random rnd = new java.util.Random();
         boolean play = true;
